07ba76d9c4da99e5f5197cf1dd2ddadde1f5a3f7
Janarth880 <Janarth.pro@gmail.com>
2024-08-13 15:10:54 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+
+router.get('/', (req, res) => {
+    res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+
+router.get('/new', (req, res) => {
+    res.render('form', { title: "New Message" });
+});
+
+
+router.post('/new', (req, res) => {
+    const messageText = req.body.messageText;
+    const messageUser = req.body.messageUser;
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect('/');
+});
+
+module.exports = router;
